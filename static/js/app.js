3e52515487b1ea33742b22e68c0851b0a310a4f8
Sam <sam.sims.13@gmail.com>
2024-12-12 17:57:13 -0600

    finished dropdown started functions

--- static/js/app.js
+++ static/js/app.js
@@ -3,19 +3,20 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+let metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-
+let sampinfo = metadata.filter((samp)=> samp.id ==sample);
 
     // Use d3 to select the panel with id of `#sample-metadata`
-
+datapanel = de.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-
+datapanel.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
+let datavals = Object.keys(sampinfo);
 
   });
 }
@@ -25,10 +26,10 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-
+let samples = data.samples;
 
     // Filter the samples for the object with the desired sample number
-
+let chartid = samples.filter((samp)=> samp.id ==sample);
 
     // Get the otu_ids, otu_labels, and sample_values
 
@@ -64,13 +65,16 @@ dropdown = d3.select("#selDataset");
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
-
+for (let i = 0;i<subnames.length;i++){
+  dropdown.append("option").text(`${subnames[i]}`).property("value", subnames[i]);
+};
 
     // Get the first sample from the list
-
+sample = subnames[0]
 
     // Build charts and metadata panel with the first sample
-
+buildMetadata(sample);
+buildCharts(sample);
   });
 }
 
